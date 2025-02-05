5859e9c3fe902bb8dbaf7beb30ab361677ab1e22
nattapong <azzzx30089@gmail.com>
2025-02-05 21:25:00 +0700

    first commit

---
+++ server/src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+require('dotenv').config();
+const port = process.env.PORT || 4000;
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// Create variable route
+const postRoute = require("./routes/post.route");
+const userRoute = require("./routes/user.route");
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// match GET localhost:9999/
+app.get("/", (req, res)=>{
+  res.send("API is online");
+});
+
+
+// Create route
+app.use("/posts", postRoute);
+app.use("/users", userRoute);
+
+
+app.listen(port, () => {
+  console.log("App started at port: " + port);
+});
